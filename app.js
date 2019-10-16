50f4879c53ba9cee11d5f38c00e2e3de66fdaebd
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-10-16 19:27:45 +0200

    Added cors and morgan

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors       = require("cors")
+const morgan     = require("morgan");
 
 const app = express();
+
+app.use(cors());
+
+if (process.env.NODE_ENV !== "test") {
+    app.use(morgan("combined"));
+}
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);

90c101ed1c936ebbaa975ce1f3b78ec80ffb9db0
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-10-23 16:56:13 +0200

    Messages for saving and fetching messages t/f db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const db = require("./db");
 
 const app = express();
 
@@ -10,7 +11,10 @@ if (process.env.NODE_ENV !== "test") {
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-io.origins(["https://jsramverk.evilbengt.me:443"]);
+io.origins([
+    "https://jsramverk.evilbengt.me:443",
+    "http://localhost:3000"
+]);
 
 io.on("connection", function (socket) {
     socket.on("messageFromClient", function (message) {
@@ -19,12 +23,29 @@ io.on("connection", function (socket) {
 
         io.emit("messageFromServer", message);
     });
+
+    socket.on("saveMessage", function (message) {
+        console.log("saveMessage:");
+        console.log("> " + message);
+
+        db.saveMessage(message);
+    });
+    socket.on("getSavedMessages", async function () {
+        console.log("getSavedMessages:");
+
+        const messages = await db.getSavedMessages();
+
+        console.log("> " + messages);
+
+        io.emit("savedMessages", messages);
+    });
+
     socket.on("clientConnected", function (message) {
         console.log("clientConnected:");
         console.log("> " + message);
 
         io.emit("newClient", message);
-    })
+    });
 });
 
 server.listen(8300);
---
+++ db.js
@@ -0,0 +1,28 @@
+const mongo = require("mongodb").MongoClient;
+
+const dsn = process.env.CHAT_DSN || "mongodb://localhost:27017/chat"
+
+async function usingCollection(dsn, colName, f) {
+    const client  = await mongo.connect(dsn);
+    const db = client.db();
+    const col = db.collection(colName);
+
+    const result = await f(col);
+
+    client.close();
+
+    return result;
+}
+
+module.exports = {
+    saveMessage: (messages) => {
+        usingCollection(dsn, "savedMessages", col => {
+            col.insert(messages);
+        });
+    },
+    getSavedMessages: async () => {
+        return await usingCollection(dsn, "savedMessages", async col => {
+            return await col.find().limit(100).toArray();
+        });
+    }
+};
